986842eb345bcef3eacf0bdd02922dfa87b1d92f
Jessie James <jess.jamestwoo11@gmail.com>
2019-09-13 15:05:45 -0600

    build added

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-var num = 2;
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = 3334
+const path = require("path")
+
+app.use('/', express.json())
+app.use('/box', require('./routes/boxRoutes'))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dddb-app', { useNewUrlParser: true })
+    .then(() => {
+        console.log('mongoose is connected')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`port is running on ${PORT}`)
+})
