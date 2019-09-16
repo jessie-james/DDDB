7b2b2fd5e1f89e0ec46a87e7d47b0b796032b37f
Jessie James <jess.jamestwoo11@gmail.com>
2019-09-16 09:21:19 -0600

    simple update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const PORT = process.env.PORT || 3334
+
 const path = require("path")
 
 app.use('/', express.json())
